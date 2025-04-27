274f9b23c40887a16f82aa35582ffb19758c346a
Enkhjee16 <enhvvshgantulga@gmail.com>
2025-04-27 09:52:23 +0200

    home page

---
+++ pages/home.php
@@ -0,0 +1,26 @@
+<h1>Welcome to Our Commercial Site!</h1>
+
+<p>We offer the best products for amazing prices. Shop with us and experience quality service!</p>
+
+<hr>
+
+<h2>Our Introduction Video (local)</h2>
+<video width="320" height="240" controls>
+  <source src="uploads/intro.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+
+<hr>
+
+<h2>Customer Testimonials (YouTube)</h2>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/YOUR_VIDEO_ID" 
+    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+</iframe>
+
+<hr>
+
+<h2>Our Location</h2>
+<iframe 
+    src="https://www.google.com/maps/embed?pb=YOUR_GOOGLE_MAPS_EMBED_CODE" 
+    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">
+</iframe>
